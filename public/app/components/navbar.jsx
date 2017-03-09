0939ebf3539d134f95e3fd65fb70ad7f1852a670
mmotes01 <mmotes@users.library.msstate.edu>
2017-03-09 16:34:31 -0600

    Working on finishing dropdown box in nav, semi-working

--- public/app/components/navbar.jsx
+++ public/app/components/navbar.jsx
@@ -2,22 +2,44 @@ import React, { Component, PropTypes } from 'react';
 export default class NavBar extends Component {
 	render(){
  
-    return(
-        <ul  className="navbar">
-         <li className="dropdown">
+    return (
+     
+      <div className="navbar row">
+        <div className="col-md-12">
+        <ul>
+         <li className="dropdown col-md-offset-3" id="nav-item">
             <a href="#" className="dropdown-btn">Clothes</a>
-            <div className="dropdown-items">  
-              <a href="#">Item</a>
-              <a href="#">Item</a>
-              <a href="#">Item</a>
-              <a href="#">Item</a>
-            </div>  
+            <div className="dropdown-items">
+              <h2 className="col-md-12">View All</h2>
+              <ul className="column1 col-md-2">  
+                <li><a href="#">Aprons</a></li>
+                <li><a href="#">Capes</a></li>
+                <li><a href="#">Dancewear</a></li>
+                <li><a href="#">Dresses</a></li>
+                <li><a href="#">Jackets</a></li>
+                <li><a href="#">Jumpsuits</a></li>
+                <li><a href="#">Nightwear</a></li>
+                <li><a href="#">Pants</a></li>
+              </ul>
+              <ul className="column2 col-md-offset-1 col-md-2">
+                <li><a href="#">Robes</a></li>
+                <li><a href="#">Shawls</a></li>
+                <li><a href="#">Shirts</a></li>
+                <li><a href="#">Shorts</a></li>
+                <li><a href="#">Skirts</a></li>
+                <li><a href="#">Suits</a></li>
+                <li><a href="#">Sweaters</a></li>
+                <li><a href="#">Vests</a></li>
+              </ul> 
+            </div> 
         </li>
-        <li>About</li>
-        <li>Policies</li>
-        <li>How To Rent</li>
-        <li>Contact</li>   
-       </ul>     
+        <li id="nav-item"><a href="#">About</a></li>
+        <li id="nav-item"><a href="#">Policies</a></li>
+        <li id="nav-item"><a href="#">How To Rent</a></li>
+        <li id="nav-item"><a href="#">Contact</a></li>   
+        </ul> 
+        </div>  
+      </div>
     );
   }
 }
